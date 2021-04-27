61fab8c9b57b75a0e10a249f6bb350a21aa49427
Slim Arias Sanhez <slimarias@github.com>
2021-04-27 18:10:38 -0500

    +ADD: handle ad statuses event when subscription has activated and expired

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -3,14 +3,23 @@
 namespace Modules\Iad\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Modules\Iad\Events\Handlers\HandleAdStatuses;
 use Modules\Iad\Events\Handlers\ProcessOrder;
 use Modules\Icommerce\Events\OrderWasProcessed;
+use Modules\Iplan\Events\SubscriptionHasFinished;
+use Modules\Iplan\Events\SubscriptionHasStarted;
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
       OrderWasProcessed::class => [
-      ProcessOrder::class
-      ]
+        ProcessOrder::class
+      ],
+      SubscriptionHasStarted::class => [
+        HandleAdStatuses::class
+      ],
+      SubscriptionHasFinished::class => [
+        HandleAdStatuses::class
+      ],
     ];
 }
