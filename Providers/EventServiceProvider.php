018a5d12d5630f7f3b6848ffc0e3842c01bf8acf
Slim Arias Sanhez <slimarias@github.com>
2021-04-28 09:55:48 -0500

    +UPDATE: external events listeners as optional if event modules are not enabled

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -5,21 +5,30 @@ namespace Modules\Iad\Providers;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Modules\Iad\Events\Handlers\HandleAdStatuses;
 use Modules\Iad\Events\Handlers\ProcessOrder;
-use Modules\Icommerce\Events\OrderWasProcessed;
-use Modules\Iplan\Events\SubscriptionHasFinished;
-use Modules\Iplan\Events\SubscriptionHasStarted;
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
-      OrderWasProcessed::class => [
-        ProcessOrder::class
-      ],
-      SubscriptionHasStarted::class => [
-        HandleAdStatuses::class
-      ],
-      SubscriptionHasFinished::class => [
-        HandleAdStatuses::class
-      ],
     ];
+
+    public function boot()
+    {
+        //Listen order was processed event
+        if(is_module_enabled('Icommerce')) {
+            Event::listen(
+                "Modules\\Icommerce\\Events\\OrderWasProcessed",
+                [ProcessOrder::class, 'handle']
+            );
+        }
+        if(is_module_enabled('Iplan')) {
+            Event::listen(
+                "Modules\\Iplan\\Events\\SubscriptionHasStarted",
+                [HandleAdStatuses::class, 'handle']
+            );
+            Event::listen(
+                "Modules\\Iplan\\Events\\SubscriptionHasFinished",
+                [HandleAdStatuses::class, 'handle']
+            );
+        }
+    }
 }
