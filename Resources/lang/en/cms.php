73d4f420b27df4c6f8ceb1c4f9421aff5f3573f2
Michael <msolano@imaginacolombia.com>
2022-02-11 19:09:03 -0500

    +Update: cms Translation files

--- Resources/lang/en/cms.php
+++ Resources/lang/en/cms.php
@@ -1,20 +1,32 @@
 <?php
+
 return [
+  "sidebar" => [
+    "adminGroup" => "Ads",
+    "adminAds" => "Ads List",
+    "adminAdsCreate" => "Create Ad",
+    "adminAdsUpdate" => "Update Ad",
+    "adminCategories" => "Ads categories",
+    "adminUps" => "Ads Ups",
+    "adminAdUps" => "Ups Log",
+    "panelAds" => "My Ads",
+    "checked" => "Checked"
+  ],
   "create" => [
     "category" => "Create category",
     "service" => "Create service",
     "ad" => "Create ad",
-    "up" => 'New Up'
+    "up" => "New Up"
   ],
   "edit" => [
     "category" => "Edit category",
     "service" => "Edit service",
     "ad" => "Edit ad",
-    "up" => 'Update Up'
+    "up" => "Update Up"
   ],
   "message" => [
-    "automaticUploads" => '¡Automatic Uploads!',
-    "chooseDefaultPrice" => 'You can select the price you want to appear by default in your ad.'
+    "automaticUploads" => "¡Automatic Uploads!",
+    "chooseDefaultPrice" => "You can select the price you want to appear by default in your ad."
   ],
   "form" => [
     "user" => "User",
@@ -29,35 +41,36 @@ return [
     "services" => "Services",
     "tags" => "Tags",
     "status" => "Status",
-    "photosAndVideo" => 'Photos and Videos',
-    "whereLocation" => 'Your Location',
-    "aboutAd" => 'About Your AD',
-    "newAd" => 'New Ad!',
-    "howContact" => 'How can contact you?',
-    "availableSchedule" => 'Available time',
-    "rates" => 'Rates',
-    "daysLimit" => 'Limit Of Days',
-    "upsDaily" => 'Ups Daily',
-    "adUps" => 'How pay your AD?',
-    "daysCounter" => 'Days Counter',
-    "upsCounter" => 'Upd Counter',
-    "fromDate" => 'From Date',
-    "toDate" => 'To Date',
-    "fromHour" => 'From Hour',
-    "toHour" => 'To Hour',
-    "nextUpAt" => 'Next Up At',
-    "rangeMinutes" => 'Go Up Every (Minutes)',
-    "featuredRate" => 'Featured Rate'
+    "photosAndVideo" => "Photos and Videos",
+    "whereLocation" => "Your Location",
+    "aboutAd" => "About Your AD",
+    "newAd" => "New Ad!",
+    "howContact" => "How can contact you?",
+    "availableSchedule" => "Available time",
+    "rates" => "Rates",
+    "daysLimit" => "Limit Of Days",
+    "upsDaily" => "Ups Daily",
+    "adUps" => "How pay your AD?",
+    "daysCounter" => "Days Counter",
+    "upsCounter" => "Upd Counter",
+    "fromDate" => "From Date",
+    "toDate" => "To Date",
+    "fromHour" => "From Hour",
+    "toHour" => "To Hour",
+    "nextUpAt" => "Next Up At",
+    "rangeMinutes" => "Go Up Every (Minutes)",
+    "featuredRate" => "Featured Rate"
   ],
   "options" => [
     "pending" => "Pending",
     "approved" => "Approved",
-    "denied" => "Denied",
+    "denied" => "Denied"
   ],
   "adCheck" => [
-    "checked" => 'Congratulations! Your ad is verified',
-    "noRequested" => 'Your ad is not verified yet, you must create a request for it to be reviewed',
-    "requested" => 'Just a little more, we are verifying your ad',
-    "rejected" => 'Your ad could not be verified, check your information and recreate the request for it to be reviewed'
+    "checked" => "Congratulations! Your ad is verified",
+    "noRequested" => "Your ad is not verified yet, you must create a request for it to be reviewed",
+    "requested" => "Just a little more, we are verifying your ad",
+    "rejected" => "Your ad could not be verified, check your information and recreate the request for it to be reviewed"
   ]
 ];
+
