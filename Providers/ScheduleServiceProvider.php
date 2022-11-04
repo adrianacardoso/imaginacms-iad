60312392cc2fe2fe17852238938c7106e396d01c
Juan Escalante <jcec007@gmail.com>
2022-11-04 12:30:53 -0500

    Update: new features multi tenant mode

--- Providers/ScheduleServiceProvider.php
+++ Providers/ScheduleServiceProvider.php
@@ -10,26 +10,25 @@ class ScheduleServiceProvider extends ServiceProvider
 {
   public function boot()
   {
-
-
+    if (!$this->app->runningInConsole()) {
+  
       $this->app->booted(function () {
         $schedule = $this->app->make(Schedule::class);
-
-        if(setting("iad::activateUploadsJob", null, false)){
+    
+        if (setting("iad::activateUploadsJob", null, false)) {
           $schedule->call(function () {
             \Modules\Iad\Jobs\UploadAds::dispatch();
           })->everyMinute();
-
+      
           $schedule->call(function () {
             \Modules\Iad\Jobs\NotifyUploadAds::dispatch();
           })->dailyAt('20:00');
         }
-
-
-
+    
+    
       });
-
-
+  
+    }
 
 
   }
