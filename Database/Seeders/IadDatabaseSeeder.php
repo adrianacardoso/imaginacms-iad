ee40b751238469036d2358b6b1b2f881fce6c31b
Cristian Marin <marin101998@gmail.com>
2023-04-26 12:17:20 -0500

    +add | modules seeds by jobs

--- Database/Seeders/IadDatabaseSeeder.php
+++ Database/Seeders/IadDatabaseSeeder.php
@@ -4,20 +4,21 @@ namespace Modules\Iad\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Isite\Jobs\ProcessSeeds;
 
 class IadDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
-
-      $this->call(IadModuleTableSeeder::class);
-      $this->call(IformComplaintTableSeeder::class);
-        // $this->call("OthersTableSeeder");
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Model::unguard();
+    ProcessSeeds::dispatch([
+      "baseClass" => "\Modules\Iad\Database\Seeders",
+      "seeds" => ["IadModuleTableSeeder", "IformComplaintTableSeeder"]
+    ]);
+  }
 }
