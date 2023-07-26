01515cf67572f496792ef9b93f803ba1dae51660
William Verde <williamverde10@gmail.com>
2023-07-26 16:39:43 -0400

    Added: validation table in handle to tenant case

--- Events/Handlers/HandleAdStatuses.php
+++ Events/Handlers/HandleAdStatuses.php
@@ -8,11 +8,16 @@ use Modules\Iad\Entities\Ad;
 class HandleAdStatuses
 {
 
+    private $log = "Iad:: Events|Handlers|HandleAdStatuses";
+
     public function handle($event){
+
+        \Log::info($this->log);
+
         $userDriver = config('asgard.user.config.driver');
         $model = $event->model;
 
-        if($model->entity === "Modules\\User\\Entities\\{$userDriver}\\User") {
+        if($model->entity === "Modules\\User\\Entities\\{$userDriver}\\User" &&  \Schema::hasTable('iad__ads')) {
 
             $adsToChange = 0;
 
